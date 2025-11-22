fcf19468d11fceb3718e5071184f99d965b5fd2f
parthmunjal07 <parth.munjal07@gmail.com>
2025-11-23 00:02:36 +0530

    more more changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,11 +3,11 @@ import ProjectCard from "./ProjectCard";
 
 function Projects() {
   return (
-    <div className="flex flex-wrap justify-center w-[65%] mx-auto">
+    <div className="flex flex-col justify-center w-[65%] mx-auto">
       <div className="w-full text-center my-20">
         <h1 className="text-5xl font-bold">My Projects</h1>
       </div>
-      <div className="flex flex-wrap justify-between">
+      <div className="flex justify-between">
         <ProjectCard
           name="Portfolio-v1"
           description="Version-1 of my personal portfolio website. In this I have tried to maintain the simplicity with focus on content I am providing instead of the animations and stuff..."
@@ -19,6 +19,14 @@ function Projects() {
           description="An online prototype for a canteen management system for colleges where students can order food online from their phones and collect it from the counter without waiting in long queues..."
           liveLink="#"
           githubLink="https://github.com/parthmunjal07/CafeMate"
+        />
+      </div>
+      <div className="flex justify-between">
+        <ProjectCard
+          name="Eventify"
+          description="Version-1 of my personal portfolio website. In this I have tried to maintain the simplicity with focus on content I am providing instead of the animations and stuff..."
+          liveLink="#"
+          githubLink="https://github.com/parthmunjal07/Eventify-Event-Tracker"
         />
       </div>
     </div>
