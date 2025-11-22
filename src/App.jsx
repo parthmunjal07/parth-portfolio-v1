2fb8f80c79815bbc7f73dadd61f9f750b59c39df
parthmunjal07 <parth.munjal07@gmail.com>
2025-11-22 22:57:29 +0530

    add url

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import About from "./components/About";
 import Blogs from "./components/Blogs";
 import ProjectCard from "./components/ProjectCard";
 
-
 function App() {
   return (
     // <BrowserRouter>
@@ -18,7 +17,12 @@ function App() {
     //     <Route path="/about" element={<About />} />
     //   </Routes>
     // </BrowserRouter>
-    <ProjectCard name="Portfolio-v1" description="Version-1 of my personal portfolio website. In this I have tried to maintain the simplicity with focus on content I am providing instead of the animations and stuff..." />
+    <ProjectCard
+      name="Portfolio-v1"
+      description="Version-1 of my personal portfolio website. In this I have tried to maintain the simplicity with focus on content I am providing instead of the animations and stuff..."
+      liveLink="#"
+      githubLink="https://github.com/parthmunjal07/parth-portfolio-v1"
+    />
   );
 }
 
