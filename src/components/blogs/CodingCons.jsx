98feec7ec39f584f79be636ea546b05043941eaf
parthmunjal07 <parth.munjal07@gmail.com>
2025-12-17 02:19:51 +0530

    blog2.2

--- src/components/blogs/CodingCons.jsx
+++ src/components/blogs/CodingCons.jsx
@@ -2,6 +2,32 @@ import React from "react";
 import PageWrapper from "../PageWrapper";
 
 export default function CodingCons() {
+    const posts = [
+    {
+      image: "/src/assets/day-0.jpg",
+      caption: "Day 0 of posting daily about coding & consistency 🚀",
+      date: "Day 0 • 14 Dec 2025",
+      postLink: "https://www.instagram.com/p/DSO-OSWD-9t/",
+    },
+    {
+      image: "/src/assets/Day-1.png",
+      caption: "Explained arrays in Java with real examples 💻",
+      date: "Day 1 • 15 Dec 2025",
+      postLink: "https://www.instagram.com/p/DSRmMdAD2Ir/?img_index=1",
+    },
+    {
+      image: "/src/assets/Day-1.png",
+      caption: "Day 2 of posting daily about coding & consistency 🚀",
+      date: "Day 2 • 16 Dec 2025",
+      postLink: "https://www.instagram.com/p/DSS_3-uE0BN/",
+    },
+    {
+      image: "/src/assets/Day-1.png",
+      caption: "Day 3 of posting daily about coding & consistency 🚀",
+      date: "Day 3 • 17 Dec 2025",
+      postLink: "https://www.instagram.com/p/DSVffW5E7no/",
+    },
+  ];
   return (
     <PageWrapper>
       <div className="bg-gray-700 w-[90%] md:w-[65%] mx-auto p-8">
