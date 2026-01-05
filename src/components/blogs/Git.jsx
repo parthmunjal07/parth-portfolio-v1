37f6e0e6bab68b285c80fcb87bf5a7669d7eea80
parthmunjal07 <parth.munjal07@gmail.com>
2026-01-06 00:07:45 +0530

    cont tom

--- src/components/blogs/Git.jsx
+++ src/components/blogs/Git.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import PageWrapper from '../PageWrapper'
 
 function Git() {
   return (
-    <div>
-      
-    </div>
+    <PageWrapper>
+          <div className="bg-gray-700 w-[90%] md:w-[65%] mx-auto p-8">
+            <div className="mt-16">
+              <h1 className="text-4xl text-center font-bold leading-tight">
+                Git - A beginners Guide
+              </h1>
+              <hr className="bg-gray-500" />
+              <h2 className="text-3xl text-center font-semibold leading-tight mt-7"> Part-1 : Why Version Control Exists: The Pen Drive Problem </h2>
+              <h3 className='text-2xl font-semibold'></h3>
+            </div>
+          </div>
+        </PageWrapper>
   )
 }
 
