46c04f574cbcc304d850970e4703ea8961356c77
parthmunjal07 <parth.munjal07@gmail.com>
2026-05-21 00:36:36 +0530

    updated and pinned socials

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import NavBar from "./components/NavBar";
+import SocialLinks from "./components/SocialLinks"; // ✅ IMPORT NEW COMPONENT
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import About from "./components/About";
 import Blogs from "./components/Blogs";
 import BlogCard from "./components/BlogCard";
 import Blog1 from "./components/blogs/Blog1";
-import CodingCons from "./components/blogs/CodingCons";
 import Git from "./components/blogs/Git";
 
 function App() {
-
   return (
     <BrowserRouter>
-    <NavBar />
-    <AnimatePresence mode="wait">
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blog1" element={<Blog1 />} />
-        <Route path="/blog2" element ={<Git />} />
-      </Routes>
+      <NavBar />
+      <SocialLinks />
+      <AnimatePresence mode="wait">
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog1" element={<Blog1 />} />
+          <Route path="/blog2" element ={<Git />} />
+        </Routes>
       </AnimatePresence>
     </BrowserRouter>
-
-    
   );
 }
 
